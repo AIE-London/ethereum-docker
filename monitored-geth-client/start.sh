c5453fae80a8c5c318ba4ca54388cf63dcb0e1d1
Duarte Aragao <daragao@gmail.com>
2017-02-03 22:39:34 +0000

    start.sh was trying to execute geth from bad directory

--- monitored-geth-client/start.sh
+++ monitored-geth-client/start.sh
@@ -3,9 +3,12 @@ set -e
 cd /root/eth-net-intelligence-api
 perl -pi -e "s/XXX/$(hostname)/g" app.json
 /usr/bin/pm2 start ./app.json
+cat ./app.json
+ls -lh /
 sleep 3
-/usr/bin/geth --datadir=~/.ethereum/devchain init "/root/files/genesis.json"
+/geth --datadir=~/.ethereum/devchain init "/root/files/genesis.json"
 sleep 3
 BOOTSTRAP_IP=`getent hosts bootstrap | cut -d" " -f1`
 GETH_OPTS=${@/XXX/$BOOTSTRAP_IP}
-/usr/bin/geth $GETH_OPTS
+/geth $GETH_OPTS
+sleep 1000
