45a4baf0d7b14f092b00877fd00934b2b531b256
Marco Spoerl <ms@marcospoerl.de>
2017-06-05 18:49:03 +0200

    Update Geth

--- monitored-geth-client/start.sh
+++ monitored-geth-client/start.sh
@@ -4,8 +4,8 @@ cd /root/eth-net-intelligence-api
 perl -pi -e "s/XXX/$(hostname)/g" app.json
 /usr/bin/pm2 start ./app.json
 sleep 3
-/geth --datadir=~/.ethereum/devchain init "/root/files/genesis.json"
+geth --datadir=~/.ethereum/devchain init "/root/files/genesis.json"
 sleep 3
 BOOTSTRAP_IP=`getent hosts bootstrap | cut -d" " -f1`
 GETH_OPTS=${@/XXX/$BOOTSTRAP_IP}
-/geth $GETH_OPTS
+geth $GETH_OPTS
