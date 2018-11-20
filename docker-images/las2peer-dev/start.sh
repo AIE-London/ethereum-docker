93e214c87cc17e79411ecf49f69e88b846bc2038
Tom Janson <tom.janson@rwth-aachen.de>
2018-11-20 03:37:40 +0100

    better bootstrap with wait-for-it

--- docker-images/las2peer-dev/start.sh
+++ docker-images/las2peer-dev/start.sh
@@ -4,11 +4,16 @@ set -e
 ETH_PROPS_DIR=/root/las2peer/etc/
 ETH_PROPS=i5.las2peer.registryGateway.Registry.properties
 
-if [ -n "$LAS2PEER_BOOTSTRAP" ]; then
-    echo Getting ethereum config from bootstrap node ...
-    L2P_BOOTSTRAP_HOST=$(echo $LAS2PEER_BOOTSTRAP | cut -d':' -f 1)
-    wget "http://$L2P_BOOTSTRAP_HOST:8001/${ETH_PROPS}" -O "${ETH_PROPS_DIR}${ETH_PROPS}"
-    echo done.
+if [ -n "$LAS2PEER_CONFIG_ENDPOINT" ]; then
+    echo Attempting to autoconfigure registry blockchain parameters ...
+    if ~/wait-for-it/wait-for-it.sh ${LAS2PEER_CONFIG_ENDPOINT} --timeout=300; then
+        echo Downloading ...
+        wget "http://${LAS2PEER_CONFIG_ENDPOINT}/${ETH_PROPS}" -O "${ETH_PROPS_DIR}${ETH_PROPS}"
+        echo done.
+    else
+        echo Registry configuration endpoint specified but not accessible. Aborting.
+        exit 1
+    fi
 fi
 
 if [ -n "$LAS2PEER_ETH_HOST" ]; then
@@ -21,20 +26,31 @@ fi
 if [ -n "$LAS2PEER_BOOTSTRAP" ]; then
     echo Skipping migration, contracts should already be deployed
 else
-    echo Waiting a bit before contract migration to ensure geth is up ...
-    # TODO: find okay timing
-    sleep 10
-    echo Starting truffle migration ...
-    cd /root/las2peer-registry-contracts
-    ./node_modules/.bin/truffle migrate --network docker_boot 2>&1 | tee migration.log
-    echo done. Setting contract addresses in config file ...
-    cat migration.log | grep '^  \w*: 0x\w*$' | sed -e 's/:/Address =/;s/^  \(.\)/\L\1/' | tail -n 3 >> "${ETH_PROPS_DIR}${ETH_PROPS}"
-    echo done. Serving config files at :8001 ...
-    cd /root/las2peer/
-    pm2 start http-server -- ./etc -p 8001
+    echo Waiting for Ethereum client ...
+    if ~/wait-for-it/wait-for-it.sh ${LAS2PEER_ETH_HOST}:8545 --timeout=300; then
+        echo Starting truffle migration ...
+        cd /root/las2peer-registry-contracts
+        ./node_modules/.bin/truffle migrate --network docker_boot 2>&1 | tee migration.log
+        echo done. Setting contract addresses in config file ...
+        cat migration.log | grep '^  \w*: 0x\w*$' | sed -e 's/:/Address =/;s/^  \(.\)/\L\1/' | tail -n 3 >> "${ETH_PROPS_DIR}${ETH_PROPS}"
+        echo done. Serving config files at :8001 ...
+        cd /root/las2peer/
+        pm2 start http-server -- ./etc -p 8001
+    else
+        echo Ethereum client not accessible. Aborting.
+        exit 2
+    fi
+fi
+
+cd /root/las2peer
+if [ -n "$LAS2PEER_BOOTSTRAP" ]; then
+    if ~/wait-for-it/wait-for-it.sh ${LAS2PEER_BOOTSTRAP} --timeout=300; then
+        echo Las2peer bootstrap available, continuing.
+    else
+        echo Las2peer bootstrap specified but not accessible. Aborting.
+        exit 3
+    fi
 fi
 
 echo Starting las2peer node ...
-cd /root/las2peer
-java -cp "registrygateway/src/main/resources/:core/export/jars/*:registrygateway/export/jars/*:restmapper/export/jars/*:webconnector/export/jars/*:core/lib/*:registrygateway/lib/*:restmapper/lib/*:webconnector/lib/*" i5.las2peer.tools.L2pNodeLauncher --port $LAS2PEER_PORT $([ -n "$LAS2PEER_BOOTSTRAP" ] && echo "--bootstrap $LAS2PEER_BOOTSTRAP") --node-id-seed $RANDOM startWebConnector interactive
-#exec /bin/sh -c "trap : TERM INT; (while true; do sleep 1000; done) & wait"
+java -cp "registrygateway/src/main/resources/:core/export/jars/*:registrygateway/export/jars/*:restmapper/export/jars/*:webconnector/export/jars/*:core/lib/*:registrygateway/lib/*:restmapper/lib/*:webconnector/lib/*" i5.las2peer.tools.L2pNodeLauncher --port $LAS2PEER_PORT $([ -n "$LAS2PEER_BOOTSTRAP" ] && echo "--bootstrap $LAS2PEER_BOOTSTRAP") --node-id-seed $RANDOM startWebConnector "node=getNode()" "registry=node.getRegistry()" "n=node" "r=registry" interactive
