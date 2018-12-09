948499bde6bf05ce283ee2b9b55867a359f91f43
Tom Janson <tom.janson@rwth-aachen.de>
2018-12-09 03:12:08 +0100

    misc (idk idc)
    # Conflicts:
    #	kubernetes-config-files/bootstrap-pod.yaml

--- docker-images/monitored-geth-client/start.sh
+++ docker-images/monitored-geth-client/start.sh
@@ -1,23 +1,43 @@
 #!/bin/bash
 set -e
+#set -o verbose # see commands as they are executed
+#set -o xtrace # see even more, including variable assignments
 
-cd /root/eth-net-intelligence-api
-perl -pi -e "s/XXX/$(hostname)/g" app.json
-/usr/bin/pm2 start ./app.json
-sleep 2
+GETH_VERBOSITY=${GETH_VERBOSITY:-"3"}
 
+if [ -n "$ETHEREUM_MONITOR" ]; then
+    if ~/wait-for-it/wait-for-it.sh $ETHEREUM_MONITOR --timeout=30; then
+        echo Connecting to Ethereum network stats monitor ...
+        cd /root/eth-net-intelligence-api
+        perl -pi -e "s/XXXXXX/$(hostname)/g" app.json
+        perl -pi -e "s/YYYYYY/$ETHEREUM_MONITOR/g" app.json
+        /usr/bin/pm2 start --silent ./app.json
+    else
+        echo Ethereum network stats monitor specified but unreachable. Ignoring and continuing.
+    fi
+fi
+
+echo Initializing blockchain from genesis file ...
 geth --datadir=~/.ethereum/devchain init "/root/files/genesis.json"
-sleep 2
+
+COMMON_OPTS="--verbosity $GETH_VERBOSITY --datadir ~/.ethereum/devchain --networkid 456719 --rpc --rpcaddr 0.0.0.0 --rpcapi db,personal,eth,net,web3,miner --rpccorsdomain=* --rpcvhosts=* --ws --wsaddr 0.0.0.0 --wsapi db,personal,eth,net,web3,miner --wsorigins=* --unlock 0,1 --password /dev/null"
 
 if [ -n "$ETHEREUM_BOOTSTRAP" ]; then
     echo Attempting to bootstrap Ethereum client ...
-    if ~/wait-for-it/wait-for-it.sh $ETHEREUM_BOOTSTRAP --timeout=300; then
+    if ~/wait-for-it/wait-for-it.sh $ETHEREUM_BOOTSTRAP --timeout=60; then
         echo Bootstrap is up, connecting ...
-        geth --datadir ~/.ethereum/devchain --networkid 456719 --bootnodes enode://288b97262895b1c7ec61cf314c2e2004407d0a5dc77566877aad1f2a36659c8b698f4b56fd06c4a0c0bf007b4cfb3e7122d907da3b005fa90e724441902eb19e@${ETHEREUM_BOOTSTRAP} --rpc --rpcaddr 0.0.0.0 --rpcapi db,personal,eth,net,web3,miner --rpccorsdomain=* --ws --wsaddr 0.0.0.0 --wsapi db,personal,eth,net,web3,miner --wsorigins=* --unlock 0,1 --password /dev/null $([ -n "$MINE" ] && echo --mine --minerthreads 1)
+
+        ETH_HOST=$(echo $ETHEREUM_BOOTSTRAP | cut -d':' -f1)
+        ETH_PORT=$(echo $ETHEREUM_BOOTSTRAP | cut -d':' -f2)
+        ETH_IP=$(getent hosts $ETH_HOST | awk '{ print $1 }')
+         
+        MINE_OPTS="$([ ${PEER_MINE:-"0"} -eq "1" ] && echo --mine --minerthreads 1)"
+        geth $COMMON_OPTS --bootnodes enode://288b97262895b1c7ec61cf314c2e2004407d0a5dc77566877aad1f2a36659c8b698f4b56fd06c4a0c0bf007b4cfb3e7122d907da3b005fa90e724441902eb19e@${ETH_IP}:${ETH_PORT}
     else
         echo Network connection to "$ETHEREUM_BOOTSTRAP" not possible, stopping.
         echo Double check that the bootstrap node is up and accessible.
     fi
 else
-    geth --datadir ~/.ethereum/devchain --networkid 456719 --nodekeyhex 091bd6067cb4612df85d9c1ff85cc47f259ced4d4cd99816b14f35650f59c322 --rpc --rpcaddr 0.0.0.0 --rpcapi db,personal,eth,net,web3,miner --rpccorsdomain=* --ws --wsaddr 0.0.0.0 --wsapi db,personal,eth,net,web3,miner --wsorigins=* --mine --minerthreads 1 --unlock 0,1 --password /dev/null
+    echo Starting new Ethereum network ...
+    geth $COMMON_OPTS --nodekeyhex 091bd6067cb4612df85d9c1ff85cc47f259ced4d4cd99816b14f35650f59c322 --mine --minerthreads 1
 fi
