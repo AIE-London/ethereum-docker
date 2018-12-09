006f992837b258163dc4ea94be341b06fba60c31
Tom Janson <tom.janson@rwth-aachen.de>
2018-12-09 20:44:37 +0100

    generation script for docker compose file with varying num of nodes

---
+++ docker-compose-files/generate.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -e
+#set -o verbose # see commands as they are executed
+
+NUMBER_NODES=${1:-5}
+NUMBER_PEERS="$(($NUMBER_NODES-1))"
+FILENAME="${2:-$NUMBER_NODES-nodes.docker-compose.yml}"
+
+echo "Generating docker compose file for a network of $NUMBER_NODES nodes (total) ..."
+echo "    That means appending the peer section $NUMBER_PEERS times to the config containing just the boot nodes."
+echo "    File '$FILENAME' will be overwritten if it already exists."
+
+cp boot-only.docker-compose.yml $FILENAME
+for (( i=1; i<=$NUMBER_PEERS; i++ )); do
+    sed "s/peer-1/peer-$i/g" peer-subsection.yml >> $FILENAME
+done
+
+echo "... done."
