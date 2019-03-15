48f483a81d1886de524d501e5a6352e079bd4aea
Tom Janson <tom.janson@rwth-aachen.de>
2019-03-15 16:20:38 +0100

    add difficulty selection to generate.sh script

--- docker-compose-files/README.md
+++ docker-compose-files/README.md
@@ -1,5 +1,7 @@
 # Docker Compose Deployment Scripts
 
+Use the `generate.sh` script to generate a Docker Compose config file. For the arguments, it’s best to look in the script. It gives quite a bit of hopefully helpful output too.
+
 ## Note about CPU limit
 
 In order to set a container CPU limit with Docker Compose, we intentionally specifiy an older 2.x version in the config file rather than the current v3 (since support for CPU limits was removed / moved to Docker Swarm).
--- docker-compose-files/generate.sh
+++ docker-compose-files/generate.sh
@@ -2,16 +2,23 @@
 set -e
 #set -o verbose # see commands as they are executed
 
-echo "Args of this script: NUMBER_NODES [FILENAME [TOTAL_CORES]]"
+echo "Args of this script: NUMBER_NODES [FILENAME [TOTAL_CORES [DIFFICULTY]]]"
+echo "    Difficulty is hardcoded via tags, the corresponding image must be available."
+echo "    E.g., the difficulty-1 tag should be available, but that actully doesn't work well."
+echo "    For the seed network using 1 CPU core (total), difficulty-400000 was used."
 
 NUMBER_NODES=${1:-5}
 NUMBER_PEERS="$(($NUMBER_NODES-1))"
 FILENAME="${2:-$NUMBER_NODES-nodes.docker-compose.yml}"
+DIFFICULTY=${3:-1}
 
 TOTAL_CORES=${3:-1}
 CPU_LIMIT=$(echo $(( 100 * $TOTAL_CORES / $NUMBER_NODES )) | sed 's/..$/.&/')
 
-echo "    Using: $NUMBER_NODES $FILENAME $TOTAL_CORES"
+echo "    Using: Number nodes: $NUMBER_NODES"
+echo "           File name: $FILENAME"
+echo "           Total CPU core limit: $TOTAL_CORES"
+echo "           Difficulty: $DIFFICULTY"
 
 echo "Generating docker compose file for a network of $NUMBER_NODES nodes (total) ..."
 echo "    That means appending the peer section $NUMBER_PEERS times to the config containing just the boot nodes."
@@ -22,9 +29,9 @@ echo "    (Version 3 removed support for this, at least for docker-compose.)"
 echo "    We divide $TOTAL_CORES CPU cores over the number of nodes, meaning $CPU_LIMIT is the 'cpus' value."
 echo "    (If you want something different, change var in script.)"
 
-sed "s/CPULIMIT/$CPU_LIMIT/g" boot-only.docker-compose.yml > $FILENAME
+sed -e "s/CPULIMIT/$CPU_LIMIT/g" -e "s/DIFFICULTY/$DIFFICULTY/g" boot-only.docker-compose.yml > $FILENAME
 for (( i=1; i<=$NUMBER_PEERS; i++ )); do
-    sed -e "s/peer-1/peer-$i/g" -e "s/CPULIMIT/$CPU_LIMIT/g" peer-subsection.yml >> $FILENAME
+    sed -e "s/peer-1/peer-$i/g" -e "s/CPULIMIT/$CPU_LIMIT/g" -e "s/DIFFICULTY/$DIFFICULTY/g" peer-subsection.yml >> $FILENAME
 done
 
 echo "... done."
