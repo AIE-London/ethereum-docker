63dd81120bbffab11102882c00b665831258160f
Michał Słupczyński <michal.slupczynski@rwth-aachen.de>
2019-08-22 18:03:37 +0200

    added terminal bell to end of migration
    indicate end of las2peer boot process

--- docker-images/las2peer/start.sh
+++ docker-images/las2peer/start.sh
@@ -64,6 +64,7 @@ else
         # yeah, this isn't fun:
         cat migration.log | grep -A5 "\(Deploying\|Replacing\|contract address\) \'\(CommunityTagIndex\|UserRegistry\|ServiceRegistry\|ReputationRegistry\)\'" | grep '\(Deploying\|Replacing\|contract address\)' | tr -d " '>:" | sed -e '$!N;s/\n//;s/Deploying//;s/Replacing//;s/contractaddress/Address = /;s/./\l&/' >> "${ETH_PROPS_DIR}${ETH_PROPS}"
         echo done. Serving config files at :8001 ...
+        echo -e "\a" # ding
         cd /app/las2peer/
         pm2 start --silent http-server -- ./etc -p 8001
     else
