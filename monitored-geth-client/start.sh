51947f24f751f00cd8d035386e22f3790363eef9
Rodrigo De Castro <rodrigo@sacaluta.com>
2022-07-19 14:38:21 -0700

    Fix failures in docker-compose.yml that prevented containers to run

--- monitored-geth-client/start.sh
+++ monitored-geth-client/start.sh
@@ -2,7 +2,7 @@
 set -e
 cd /root/eth-net-intelligence-api
 perl -pi -e "s/XXX/$(hostname)/g" app.json
-/usr/bin/pm2 start ./app.json
+/usr/local/bin/pm2 start ./app.json
 sleep 3
 geth --datadir=~/.ethereum/devchain init "/root/files/genesis.json"
 sleep 3
